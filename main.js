3445b10c84790e99cec6186b77dc4e1a05f5579c
sshch3612 <1758974924@qq.com>
2020-04-22 23:44:34 +0800

    init

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
+  // mainWindow.loadURL('https://www.kancloud.cn/marswill/dark2_document/709098')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
